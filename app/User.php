33f53061407f5389dfbb217064b5c50531ae4f5d
Ross Bartlett <rossjbartlett@gmail.com>
2019-03-07 14:59:04 -0700

    Initial commit

---
+++ app/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use App\Course;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'role', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    //when you call courses() from a User object, it returns all the Course objects of the User
+    public function courses(){
+        return $this->hasMany(Course::class);
+    }
+}
