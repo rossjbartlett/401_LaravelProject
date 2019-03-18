6119ef7c227718d4250c84c266eedf197b269cf1
Dylan <dylan.gordon.10@gmail.com>
2019-03-18 13:42:50 -0600

    Changed viewing book privileges

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -25,9 +25,13 @@
     </div>
     <div class="col-md-10">
         <h2>
+            @if((Auth::user() != null) && (Auth::user()->isAdmin() || Auth::user()->isSubscriber()))
             <a href="{{action('BookController@show',[$book->id])}}">
                 {{$book->name}}
             </a>
+            @else
+                {{$book->name}}
+            @endif
         </h2>
         {{$book->publisher}}, {{$book->publication_year}}
     </div>
