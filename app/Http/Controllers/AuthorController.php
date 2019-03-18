6be4ebd5ae7e14ddf0ab9205ebe855f44c9bef39
Dylan <dylan.gordon.10@gmail.com>
2019-03-18 16:47:56 -0600

    Added login requirement to view authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+
+    public function __construct()
+    {
+      $this->middleware('user');
+    }
+
     /**
      * Display a listing of the resource.
      *
