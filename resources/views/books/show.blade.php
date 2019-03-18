ac0e06fe6252c28a4c99e32159c32c5c6b39717d
Alexa Astorino <alexa.astorino@gmail.com>
2019-03-18 17:28:47 -0600

    added create button if an admin

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -10,18 +10,24 @@
         @if(Auth::check())
 
           @if(Auth::user()->isAdmin())
-            {!! Form::model($book, ['method'=>'DELETE', 'action'=>['BookController@destroy',$book->id]]) !!}
-            {!! Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm', 'style' => 'float:right']) !!}
-            {!! Form::close() !!}
+              <div class="btn-group-vertical" style="float:right">
+                  {!! Form::model($book, ['method'=>'DELETE', 'action'=>['BookController@destroy',$book->id]]) !!}
+                  {!! Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm']) !!}
+                  {!! Form::close() !!}
+                  <br>
+                  <a href="{{action('BookController@create')}}">
+                      <button type="button" class="btn btn-primary btn-sm" style="float:right"> Edit </button>
+                  </a>
+              </div>
           @elseif(Auth::user()->isSubscriber())
-            @if( Auth::user()->isSubscribed($book->id))
-              <!-- TODO do we want to unsubscribe -->
-            @else
-              {!! Form::open(['method' => 'POST', 'url' => 'subscriptions']) !!}
-              <input id='book_id' name = 'book_id' type = 'hidden' value = {{$book->id}}>
-              {!! Form::submit('Subscribe', ['class' => 'btn btn-primary', 'style' => 'float:right']) !!}
-              {!! Form::close() !!}
-            @endif
+              @if( Auth::user()->isSubscribed($book->id))
+                  <!-- TODO do we want to unsubscribe -->
+              @else
+                  {!! Form::open(['method' => 'POST', 'url' => 'subscriptions']) !!}
+                  <input id='book_id' name = 'book_id' type = 'hidden' value = {{$book->id}}>
+                  {!! Form::submit('Subscribe', ['class' => 'btn btn-primary', 'style' => 'float:right']) !!}
+                  {!! Form::close() !!}
+              @endif
           @endif
 
         @endif
@@ -36,14 +42,18 @@
         <hr>
 
 <!-- TODO: make it so it only subscribed users can comment??? -->
-    @if(Auth::user()->isSubscriber() && Auth::user()->isSubscribed($book->id))
-      <h4>Add comments:</h4>
-      {!! Form::open(['action'=>'CommentController@store']) !!}
-      <p>{!! Form::textarea('text', null, ['class'=>'form-control']) !!}</p>
-      {{ Form::hidden('book_id', $book->id) }}
-      <p>{{ Form::submit('Post Comment') }}</p>
-      {!! Form::close() !!}
-    @endif
+      @if(Auth::check())
+
+          @if(Auth::user()->isSubscriber() && Auth::user()->isSubscribed($book->id))
+              <h4>Add comments:</h4>
+              {!! Form::open(['action'=>'CommentController@store']) !!}
+              <p>{!! Form::textarea('text', null, ['class'=>'form-control']) !!}</p>
+              {{ Form::hidden('book_id', $book->id) }}
+              <p>{{ Form::submit('Post Comment') }}</p>
+              {!! Form::close() !!}
+          @endif
+
+      @endif    
 
 
       <h4 style="display:inline"> Posted Comment(s): </br> </h4>
